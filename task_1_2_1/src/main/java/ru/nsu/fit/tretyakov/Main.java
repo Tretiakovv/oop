80cee3c0291e333f6893b97dad11779b794614cb
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2022-09-28 17:23:29 +0700

    Update | Change Jacoco reports

--- task_1_2_1/src/main/java/ru/nsu/fit/tretyakov/Main.java
+++ task_1_2_1/src/main/java/ru/nsu/fit/tretyakov/Main.java
@@ -1,28 +1,7 @@
 package ru.nsu.fit.tretyakov;
 
-class Parent{
-    Parent(){
-        System.out.print("P");
-    }
-    public void doIt(){
-        System.out.print("A");
-    }
-}
-
-class Child extends Parent{
-    Child(){
-        super();
-        System.out.println("C");
-    }
-
-    public void doIt() {
-        System.out.println("B");
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Parent parent = new Child();
-        parent.doIt();
+        System.out.println("Hello, world!");
     }
 }

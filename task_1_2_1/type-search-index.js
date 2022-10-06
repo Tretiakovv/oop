aa1cf6d251086872970af857eee72371f7e730bc
Tretiakovv <Tretiakovv@users.noreply.github.com>
2022-10-06 02:29:13 +0000

    Deploying to gh-pages from @ Tretiakovv/oop@6791c6cf23bdbbbba995e6cc32d04cfa8a0cd7f7 🚀

--- task_1_2_1/type-search-index.js
+++ task_1_2_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.fit.tretyakov","l":"Main"},{"p":"ru.nsu.fit.tretyakov","l":"Stack"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.fit.tretyakov","l":"Stack"}]

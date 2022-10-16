a1f4b318e4522e40b52e1c1f17c87f23082c0c5c
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2022-10-16 22:26:37 +0700

    Update | Edited many methods and whole logic of the program
    I implemented many methods and made many tests. Unfortunately, my code has problems, so i'll fix them lately

--- task_1_2_2/src/main/java/ru/nsu/fit/tretyakov/Tree.java
+++ task_1_2_2/src/main/java/ru/nsu/fit/tretyakov/Tree.java
@@ -1,28 +1,23 @@
 package ru.nsu.fit.tretyakov;
 
-import java.io.BufferedInputStream;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.function.IntFunction;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
+import java.util.*;
+
+import static java.lang.Math.max;
 
 public class Tree<T> implements Collection<T> {
 
     private int size;
     private Node<T> root;
-    private Node<T>[] nodes;
 
     public Tree(int size, T data) {
         this.size = size;
-        this.root = new Node<>(data);
+        this.root = new Node<>(data,null);
     }
 
-    private boolean findData(Node<T> curNode, T data){
+    private boolean findData(Node<T> curNode, T data) {
 
         if (curNode == null) return false;
-        if (curNode.getData() == data) return true;
+        if (curNode.data == data) return true;
 
         Node<T> ls = curNode.leftSon;
         Node<T> rs = curNode.rightSon;
@@ -30,53 +25,118 @@ public class Tree<T> implements Collection<T> {
         return findData(ls, data) || findData(rs, data);
     }
 
-    private void createArrayFromTree(Object[] resultArray, int sizeOfArray, Node<T> node){
-        if (node == null) return;
-        resultArray[sizeOfArray++] = (Object) node;
-        createArrayFromTree(resultArray,sizeOfArray,node.leftSon);
-        createArrayFromTree(resultArray,sizeOfArray,node.rightSon);
+    private int createArrayFromTree(Object[] resultArray, int sizeOfArray, Node<T> node) {
+        if (node != null){
+            resultArray[sizeOfArray++] = node.data;
+            sizeOfArray = createArrayFromTree(resultArray, sizeOfArray, node.leftSon);
+            sizeOfArray = createArrayFromTree(resultArray, sizeOfArray, node.rightSon);
+        }
+        return sizeOfArray;
     }
 
-    private void addRecursive(Node<T> node, T data){
+    private Node<T> addRecursive(Node<T> node, T data) {
+
+        if (node.leftSon == null || node.rightSon == null) {
+
+            size++;
+            var sonDepth = node.depth + 1;
+
+            if (node.leftSon == null) node.leftSon = new Node<T>(data,sonDepth, node);
+            else node.rightSon = new Node<T>(data, sonDepth, node);
+
+            node.weight++;
+            return node;
+
+        } else if (node.leftSon.weight <= node.rightSon.weight)
+            addRecursive(node.leftSon, data);
+        else addRecursive(node.rightSon, data);
+
+        node.weight = max(node.leftSon.weight, node.rightSon.weight) + 1;
+        return node;
+    }
+
+    private Node<T> clearTraverse(Node<T> node) {
+        if (node.leftSon != null) node.leftSon = clearTraverse(node.leftSon);
+        if (node.rightSon != null) node.rightSon = clearTraverse(node.rightSon);
+        node = null;
+        return node;
+    }
+
+    private Node<T> findDeepestLeaf(Node<T> node, int maxDepth) {
 
         Node<T> ls = node.leftSon;
         Node<T> rs = node.rightSon;
 
-        if (ls == null || rs == null){
-            if (ls == null) ls = new Node<T>(data);
-            else rs = new Node<T>(data);
-            size++;
+        // if node is leaf
+
+        if (ls == null && rs == null) {
+            if (node.depth >= maxDepth) {
+                maxDepth = node.depth;
+                return node;
+            }
         }
 
-        else if (ls.depth <= rs.depth) addRecursive(ls,data);
-        else addRecursive(rs,data);
+        // return deepest leaf
 
-        return;
+        if (rs != null && ls != null) {
+
+            Node<T> deepestRight, deepestLeft;
+
+            deepestRight = findDeepestLeaf(rs, maxDepth);
+            deepestLeft = findDeepestLeaf(ls, maxDepth);
+
+            if (deepestRight.depth >= deepestLeft.depth)
+                return deepestRight;
+            else return deepestLeft;
+
+        } else return findDeepestLeaf(Objects.requireNonNullElse(rs, ls), maxDepth);
     }
 
-    private void clearTraverse(Node<T> node){
-        if (node.leftSon == null && node.rightSon == null){
-            node = null;
-            return;
+    private Node<T> removeRecursive(Node<T> curNode, T data) {
+
+        Node<T> ls = curNode.leftSon;
+        Node<T> rs = curNode.rightSon;
+        Node<T> prevNode = curNode;
+
+        if (curNode.data == data) {
+            if (ls == null && rs == null) curNode = null;
+            else {
+                curNode = findDeepestLeaf(root, 0);
+
+                if (curNode.father.leftSon != null){
+                    if (curNode.father.leftSon.data == curNode.data){
+                        curNode.father.leftSon = null;
+                    }
+                } else if (curNode.father.rightSon != null){
+                    if (curNode.father.rightSon.data == curNode.data){
+                        curNode.father.rightSon = null;
+                    }
+                }
+
+                curNode.father = prevNode.father;
+                curNode.depth = prevNode.depth + 1;
+
+                if (ls != curNode) curNode.leftSon = ls;
+                else curNode.leftSon = null;
+                if (rs != curNode) curNode.rightSon = rs;
+                else curNode.rightSon = null;
+
+                if (curNode.rightSon != null && curNode.leftSon != null)
+                    curNode.weight = max(curNode.leftSon.weight,curNode.rightSon.weight) + 1;
+                else if (curNode.leftSon != null)
+                    curNode.weight = curNode.leftSon.weight + 1;
+                else if (curNode.rightSon != null)
+                    curNode.weight = curNode.rightSon.weight + 1;
+                else curNode.weight = 0;
+            }
+
+        } else{
+            if (curNode.leftSon != null) curNode.leftSon = removeRecursive(curNode.leftSon,data);
+            if (curNode.rightSon != null) curNode.rightSon = removeRecursive(curNode.rightSon,data);
         }
-        if (node.leftSon != null) clearTraverse(node.leftSon);
-        if (node.rightSon != null) clearTraverse(node.rightSon);
-    }
 
-//    private Node<T> remove(Node<T> curNode, Node<T> checkNode){
-//
-//        Node<T> ls = curNode.leftSon;
-//        Node<T> rs = curNode.rightSon;
-//
-//        /*if (checkNode.equals(curNode)){
-//            if (ls == null && rs == null) curNode = null;
-//            else{
-//
-//            }
-//
-//        }*/
-//
-//    }
+        return curNode;
+    }
 
     @Override
     public int size() {
@@ -90,29 +150,20 @@ public class Tree<T> implements Collection<T> {
 
     @SuppressWarnings("unchecked")
     @Override
-    public boolean contains(Object o) {
-        return findData(root,(T)o);
+    public boolean contains(Object o) throws NullPointerException{
+        if (o == null) throw new NullPointerException("Elements is equals null");
+        return findData(root, (T) o);
     }
 
     @Override
     public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            @Override
-            public boolean hasNext() {
-                return false;
-            }
-
-            @Override
-            public T next() {
-                return null;
-            }
-        };
+        return new TreeIterator<T>(this.root) {};
     }
 
     @Override
     public Object[] toArray() {
         Object[] resultArray = new Object[size];
-        createArrayFromTree(resultArray,0,root);
+        createArrayFromTree(resultArray, 0, root);
         return resultArray;
     }
 
@@ -120,26 +171,29 @@ public class Tree<T> implements Collection<T> {
     @Override
     public <T1> T1[] toArray(T1[] a) {
         T1[] resultArray = (T1[]) new Object[size];
-        createArrayFromTree(resultArray,0,root);
+        createArrayFromTree(resultArray, 0, root);
         return resultArray;
     }
 
     @Override
     public boolean add(T t) {
-        addRecursive(root,t);
+        root = addRecursive(root, t);
         return true;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public boolean remove(Object o) throws NullPointerException{
+    public boolean remove(Object o) throws NullPointerException {
         if (!contains(o)) return false;
+        root = removeRecursive(root,(T)o);
+        size--;
         return true;
     }
 
     @Override
     public boolean containsAll(Collection<?> c) {
-        while(c.iterator().hasNext()){
-            if (!contains(c.iterator().next()))
+        for (Object o : c) {
+            if (!contains(o))
                 return false;
         }
         return true;
@@ -147,43 +201,100 @@ public class Tree<T> implements Collection<T> {
 
     @Override
     public boolean addAll(Collection<? extends T> c) {
-        return false;
+        for(T o: c) add(o);
+        return true;
     }
 
+    // there's exception when element of collection isn't in tree
     @Override
     public boolean removeAll(Collection<?> c) {
-        return false;
+        for(Object o: c) remove(o);
+        return true;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
-    public boolean retainAll(Collection<?> c) {
-        return false;
+    public boolean retainAll(Collection<?> c) throws NullPointerException{
+
+        Tree<T> newTree = null;
+        boolean allHasBeenRetained = true;
+        boolean hasRoot = false;
+
+        for(Object o: c){
+            if (!contains(o)) allHasBeenRetained = false;
+            else{
+                if (o == null){
+                    throw new NullPointerException("Cannot retain null-element in collection");
+                }
+                if (!hasRoot){
+                    newTree = new Tree<>(1, (T)o);
+                    hasRoot = true;
+                }
+                else newTree.add((T)o);
+            }
+        }
+
+        if (newTree != null){
+            root = newTree.root;
+            size = newTree.size();
+
+        }
+        return allHasBeenRetained;
     }
 
     @Override
     public void clear() {
-        clearTraverse(root);
+        root = clearTraverse(root);
+        size = 0;
     }
 
-    private class Node<T>{
+    private static class TreeIterator<T> implements Iterator<T>{
+        private List<Node<T>> treeNodes;
+        private Integer cursor;
+
+        private TreeIterator(Node<T> root) {
+            this.treeNodes = new ArrayList<>();
+            cursor = 0;
+            createArrayList(treeNodes,root);
+        }
+
+        private void createArrayList(List<Node<T>> list, Node<T> node){
+            if (node == null) return;
+            list.add(node);
+            createArrayList(list,node.leftSon);
+            createArrayList(list,node.rightSon);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return cursor != treeNodes.size();
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) throw new ConcurrentModificationException();
+            return treeNodes.get(cursor++).data;
+        }
+    }
+
+    private static class Node<T> {
+        protected Integer weight, depth;
+        protected Node<T> leftSon, rightSon, father;
         private T data;
-        protected Integer depth;
-        protected boolean isChecked;
-        protected Node<T> leftSon, rightSon;
 
-        public T getData() {
-            return data;
-        }
-
-        public void setData(T data) {
+        public Node(T data, Node<T> father) {
             this.data = data;
-        }
-
-        public Node(T data) {
-            this.data = data;
-            isChecked = false;
-            depth = 0;
+            depth = weight = 0;
             leftSon = rightSon = null;
+            this.father = father;
+        }
+
+        public Node(T data, Integer depth, Node<T> father) {
+            this.data = data;
+            this.depth = depth;
+            weight = 0;
+            leftSon = rightSon = null;
+            this.father = father;
         }
     }
 }
--- task_1_2_2/src/test/java/ru/nsu/fit/tretyakov/TreeTest.java
+++ task_1_2_2/src/test/java/ru/nsu/fit/tretyakov/TreeTest.java
@@ -2,9 +2,17 @@ package ru.nsu.fit.tretyakov;
 
 import org.junit.jupiter.api.Test;
 import ru.nsu.fit.tretyakov.Tree;
+
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class TreeTest {
+
+    // size test
 
     @Test
     public void getSizeTest(){
@@ -13,6 +21,69 @@ public class TreeTest {
         simpleTree.add("ccc");
         assertEquals(simpleTree.size(),3);
     }
+
+    // adding tests
+
+    @Test
+    public void addSingleTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        Integer[] tstArray = {1,2,3};
+        integerTree.add(2);
+        integerTree.add(3);
+        assertArrayEquals(integerTree.toArray(),tstArray);
+    }
+
+    @Test
+    public void addAllTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        Integer[] tstArray = {1,2,3};
+        integerTree.addAll(List.of(new Integer[]{2,3}));
+        assertArrayEquals(integerTree.toArray(),tstArray);
+    }
+
+    // removing tests
+
+    @Test
+    public void removeRootSingleTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        integerTree.remove(1);
+        assertFalse(integerTree.iterator().hasNext());
+    }
+
+    @Test
+    public void removeNodeSingleWithChangingRootTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        integerTree.addAll(List.of(new Integer[]{2,3,4,5}));
+        integerTree.remove(2);
+        integerTree.remove(3);
+        integerTree.remove(4);
+        integerTree.remove(1);
+        assertEquals(integerTree.toArray()[0],5);
+    }
+
+    @Test
+    public void removeAllTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        integerTree.addAll(List.of(new Integer[]{2,3,4,5}));
+        integerTree.removeAll(List.of(new Integer[]{2,3,4,5}));
+        assertEquals(integerTree.toArray()[0],1);
+    }
+
+    @Test
+    public void removeElemNotInTreeTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        assertFalse(integerTree.remove(5));
+    }
+
+    @Test
+    public void removeNullTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        assertThrows(NullPointerException.class,()-> {
+            integerTree.remove(null);
+        });
+    }
+
+    // contains tests
 
     @Test
     public void containsTest(){
@@ -25,5 +96,90 @@ public class TreeTest {
         Tree<String> simpleTree = new Tree<>(1,"aaa");
         assertFalse(simpleTree.contains("bbb"));
     }
+
+    @Test
+    public void containsNullTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        assertThrows(NullPointerException.class,()-> {
+            integerTree.contains(null);
+        });
+    }
+
+    // retain tests
+
+    @Test
+    public void retainAllSimpleTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        integerTree.addAll(Stream.of(2,3,4,5,6).toList());
+        integerTree.retainAll(Stream.of(2,3).toList());
+        assertArrayEquals(integerTree.toArray(),Stream.of(2,3).toArray());
+    }
+
+    @Test
+    public void retainAllNullTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        integerTree.addAll(Stream.of(2,3,4,5,6).toList());
+        assertThrows(NullPointerException.class,()-> {
+            integerTree.retainAll(Stream.of(2,null).toList());
+        });
+    }
+
+    // iterator tests
+
+    @Test
+    public void iteratorHasNextTrueTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        assertTrue(integerTree.iterator().hasNext());
+    }
+
+    @Test
+    public void iteratorHasNextFalseTest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        integerTree.remove(1);
+        assertFalse(integerTree.iterator().hasNext());
+    }
+
+    @Test
+    public void iteratorIterateCMETest(){
+        Tree<Integer> integerTree = new Tree<>(1,1);
+        integerTree.addAll(Stream.of(2,3,4,5).toList());
+
+        Iterator<Integer> itr = integerTree.iterator();
+        while(itr.hasNext()){
+            itr.next();
+        }
+
+        assertThrows(ConcurrentModificationException.class, itr::next);
+    }
+
+    // clear tests
+
+    @Test
+    public void clearTest(){
+        Tree<Integer> simpleTree = new Tree<>(1,1);
+        simpleTree.addAll(Stream.of(2,3,4,5).toList());
+        simpleTree.clear();
+        assertThat(simpleTree.toArray()).isEmpty();
+        assertTrue(simpleTree.isEmpty());
+    }
+
+    // to array test
+
+    @Test
+    public void toArray(){
+        Tree<Integer> simpleTree = new Tree<>(1,1);
+        simpleTree.addAll(Stream.of(2,3,4,5).toList());
+        Object[] finalArray = simpleTree.toArray();
+        assertArrayEquals(finalArray,new Object[]{1,2,4,3,5});
+    }
+
+    @Test
+    public void toArrayGeneric(){
+        Tree<Integer> simpleTree = new Tree<>(1,1);
+        simpleTree.addAll(Stream.of(2,3,4,5).toList());
+        Integer[] finalArray = simpleTree.to
+        assertArrayEquals(finalArray,new Object[]{1,2,4,3,5});
+    }
+
 
 }

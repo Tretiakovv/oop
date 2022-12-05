b09849b54053aa7af488828474eac3c07f71bc3a
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2022-12-05 14:56:26 +0700

    Fix | Fixed javadocs

--- task_1_4_1/src/test/java/ru/nsu/fit/tretyakov/GradebookTest.java
+++ task_1_4_1/src/test/java/ru/nsu/fit/tretyakov/GradebookTest.java
@@ -10,7 +10,7 @@ public class GradebookTest {
 
     @BeforeEach
     public void initGradebook() {
-        this.gb = new Gradebook("Tretyakov Artem","21214","210630");
+        this.gb = new Gradebook("Tretyakov Artem", "21214", "210630");
         gb.addSemester(1);
     }
 
@@ -114,47 +114,47 @@ public class GradebookTest {
     }
 
     @Test
-    public void creditsExceptions(){
-        assertThrows(IllegalStateException.class,()->{
-            gb.addSubject(1,"Math","AAA");
+    public void creditsExceptions() {
+        assertThrows(IllegalStateException.class, () -> {
+            gb.addSubject(1, "Math", "AAA");
         });
-        assertThrows(IllegalStateException.class,()->{
-            gb.addSubject(1,"Math","UndifCredit");
-            gb.setGrade(1,"Math",3);
+        assertThrows(IllegalStateException.class, () -> {
+            gb.addSubject(1, "Math", "UndifCredit");
+            gb.setGrade(1, "Math", 3);
         });
-        assertThrows(IllegalStateException.class,()->{
-            gb.addSubject(1,"Math","Exam");
-            gb.setGrade(1,"Math",6);
+        assertThrows(IllegalStateException.class, () -> {
+            gb.addSubject(1, "Math", "Exam");
+            gb.setGrade(1, "Math", 6);
         });
     }
 
     @Test
-    public void printGradebook(){
-        gb.addSubject(1,"Math","Exam");
-        gb.addSubject(1,"OOP","Exam");
-        gb.addSubject(1,"Theory of probability","Exam");
-        gb.addSubject(1,"Imperative programming","Exam");
-        gb.addSubject(1,"History","DifCredit");
+    public void printGradebook() {
+        gb.addSubject(1, "Math", "Exam");
+        gb.addSubject(1, "OOP", "Exam");
+        gb.addSubject(1, "Theory of probability", "Exam");
+        gb.addSubject(1, "Imperative programming", "Exam");
+        gb.addSubject(1, "History", "DifCredit");
 
-        gb.setGrade(1,"Math",5);
-        gb.setGrade(1,"Theory of probability",4);
-        gb.setGrade(1,"Imperative programming",5);
-        gb.setGrade(1,"OOP",5);
-        gb.setGrade(1,"History",4);
+        gb.setGrade(1, "Math", 5);
+        gb.setGrade(1, "Theory of probability", 4);
+        gb.setGrade(1, "Imperative programming", 5);
+        gb.setGrade(1, "OOP", 5);
+        gb.setGrade(1, "History", 4);
 
         gb.addSemester(2);
 
-        gb.addSubject(2,"Declarative programming","DifCredit");
-        gb.addSubject(2,"English","DifCredit");
-        gb.addSubject(2,"Team project","DifCredit");
-        gb.addSubject(2,"Physics","DifCredit");
-        gb.addSubject(2,"PE","UndifCredit");
+        gb.addSubject(2, "Declarative programming", "DifCredit");
+        gb.addSubject(2, "English", "DifCredit");
+        gb.addSubject(2, "Team project", "DifCredit");
+        gb.addSubject(2, "Physics", "DifCredit");
+        gb.addSubject(2, "PE", "UndifCredit");
 
-        gb.setGrade(2,"Declarative programming",5);
-        gb.setGrade(2,"English",4);
-        gb.setGrade(2,"Team project",5);
-        gb.setGrade(2,"Physics",4);
-        gb.setGrade(2,"PE",5);
+        gb.setGrade(2, "Declarative programming", 5);
+        gb.setGrade(2, "English", 4);
+        gb.setGrade(2, "Team project", 5);
+        gb.setGrade(2, "Physics", 4);
+        gb.setGrade(2, "PE", 5);
 
         gb.printGradebook();
     }

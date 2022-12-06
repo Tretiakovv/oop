e4bc2a7f6893b044f37ba153f4352b14889eb2e4
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2022-12-06 14:13:46 +0700

    Update | Resolved all comments
    I made another inner class — Semester, which contains average grade point for all semesters in gradebook. Also added enum-class for credit type.

--- task_1_4_1/src/main/java/ru/nsu/fit/tretyakov/Gradebook.java
+++ task_1_4_1/src/main/java/ru/nsu/fit/tretyakov/Gradebook.java
@@ -1,24 +1,22 @@
 package ru.nsu.fit.tretyakov;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.*;
 
 /**
  * This class is the student's grade book. In it user can get
  * information about his score, subjects, average grade point and
- * his honor diploma.
+ * his honor diploma. User can also get list of all subjects with
+ * specific credit type.
  */
 public class Gradebook {
 
-    private final Map<Integer, HashMap<String, Subject>> gradeBook;
-    /**
-     * Allowed credit types of grade book. Used to determine values of credits.
-     */
-    private final Set<String> typesOfCredit;
+    private final Map<Integer, Semester> gradeBook;
     private final String name;
     private final String group;
     private final String numberOfGradeBook;
-    private Float averageGradePoint;
-    private boolean scholarship, honorDiploma;
+    private boolean honorDiploma;
     private Integer subjects, thesis;
 
     /**
@@ -33,11 +31,8 @@ public class Gradebook {
         this.group = group;
         this.numberOfGradeBook = numberOfGradeBook;
         this.gradeBook = new HashMap<>();
-        this.scholarship = this.honorDiploma = false;
-        this.averageGradePoint = 0.f;
+        this.honorDiploma = false;
         this.subjects = this.thesis = 0;
-        String[] creditTypes = new String[]{"Exam", "DifCredit", "UndifCredit"};
-        this.typesOfCredit = new HashSet<>(List.of(creditTypes));
     }
 
     /**
@@ -51,7 +46,7 @@ public class Gradebook {
         if (gradeBook.get(semester) != null) {
             return false;
         }
-        gradeBook.put(semester, new HashMap<>());
+        gradeBook.put(semester, new Semester());
         return true;
     }
 
@@ -65,18 +60,16 @@ public class Gradebook {
      * @throws IllegalStateException if there is already exist a semester
      *                               or subject with specific value.
      */
-    public void addSubject(Integer semester, String subject, String creditType) throws IllegalStateException {
-        if (!typesOfCredit.contains(creditType)) {
-            throw new IllegalStateException("This credit type doesn't contained in grade book");
-        }
+    public void addSubject(Integer semester, String subject, CreditType creditType) throws IllegalStateException {
         if (gradeBook.get(semester) == null) {
             throw new IllegalStateException("This semester doesn't exist in grade book");
         }
-        if (gradeBook.get(semester).get(subject) != null) {
+        if (gradeBook.get(semester).semesterSubjects.get(subject) != null) {
             throw new IllegalStateException("This subject is already exist in grade book");
         }
-        gradeBook.get(semester).put(subject, new Subject(creditType));
+        gradeBook.get(semester).semesterSubjects.put(subject, new Subject(creditType));
         this.subjects++;
+        gradeBook.get(semester).amountOfSubjects++;
     }
 
     /**
@@ -88,8 +81,8 @@ public class Gradebook {
      * @throws IllegalStateException if required semester or grade are don't exist in the grade book.
      */
     public Integer getGrade(Integer semester, String subject) throws IllegalStateException {
-        handleSubject(semester, subject);
-        return gradeBook.get(semester).get(subject).grade;
+        validateSubject(semester, subject);
+        return gradeBook.get(semester).semesterSubjects.get(subject).grade;
     }
 
     /**
@@ -101,21 +94,36 @@ public class Gradebook {
      * @throws IllegalStateException if the passed value isn't validated.
      */
     public void setGrade(Integer semester, String subject, Integer value) throws IllegalStateException {
-        handleSubject(semester, subject);
-        var curSubject = gradeBook.get(semester).get(subject);
+        validateSubject(semester, subject);
+        var curSubject = gradeBook.get(semester).semesterSubjects.get(subject);
 
-        if (Objects.equals(curSubject.creditType, "UndifCredit")) {
+        if (curSubject.creditType == CreditType.UNDIF_CREDIT) {
             if (value == 2 || value == 5) {
-                gradeBook.get(semester).get(subject).grade = value;
+                gradeBook.get(semester).semesterSubjects.get(subject).grade = value;
             } else {
                 throw new IllegalStateException("Undifferentiated credit grade cannot be evaluated by this value");
             }
         } else if (value >= 2 && value <= 5) {
-            gradeBook.get(semester).get(subject).grade = value;
+            gradeBook.get(semester).semesterSubjects.get(subject).grade = value;
         } else {
             throw new IllegalStateException("Subject cannot be evaluated by this value");
         }
-        computeScore();
+        gradeBook.get(semester).computeScore();
+    }
+
+    /**
+     * Getter of all subjects in specific semester with specific credit type
+     *
+     * @param semester is the required semester where user needs to find subjects
+     * @param type     is the required credit type of subject
+     * @return ArrayList of all subjects in current semester with passed credit type
+     */
+    public List<Subject> getSubjectsByType(Integer semester, CreditType type) {
+        List<Subject> subjectList = new ArrayList<>();
+        for (var subject : gradeBook.get(semester).semesterSubjects.values()) {
+            if (subject.creditType == type) subjectList.add(subject);
+        }
+        return subjectList;
     }
 
     /**
@@ -130,18 +138,19 @@ public class Gradebook {
 
         System.out.println("-------------------------------\n");
 
-        System.out.printf("Student's average grade point: %f\n", this.averageGradePoint);
-        if (this.honorDiploma) System.out.println("Student's honor diploma: YES");
-        else System.out.println("Student's honor diploma: NO");
+        System.out.printf("Student's honor diploma: %s\n", this.honorDiploma ? "YES" : "NO");
 
         System.out.println("-------------------------------\n");
 
         for (var semester : gradeBook.keySet()) {
             System.out.printf("Semester #%d\n\n", semester);
+            System.out.printf("Student's average grade point: %f\n",
+                    gradeBook.get(semester).averageGradePoint);
             System.out.println("-------------------------------\n");
-            for (var subject : gradeBook.get(semester).keySet()) {
-                var curSubject = gradeBook.get(semester).get(subject);
-                System.out.printf("Subject: %s   |   Credit type: %s   |   Grade: %d\n\n", subject, curSubject.creditType, curSubject.grade);
+            for (var subject : gradeBook.get(semester).semesterSubjects.keySet()) {
+                var curSubject = gradeBook.get(semester).semesterSubjects.get(subject);
+                System.out.printf("Subject: %s   |   Credit type: %s   |   Grade: %d\n\n", subject,
+                        curSubject.creditType, curSubject.grade);
             }
         }
 
@@ -153,9 +162,9 @@ public class Gradebook {
         var countFive = 0;
         this.honorDiploma = true;
 
-        for (var courses : gradeBook.keySet()) {
-            for (var subjects : gradeBook.get(courses).keySet()) {
-                var grade = gradeBook.get(courses).get(subjects).grade;
+        for (var semester : gradeBook.keySet()) {
+            for (var subject : gradeBook.get(semester).semesterSubjects.keySet()) {
+                var grade = gradeBook.get(semester).semesterSubjects.get(subject).grade;
                 if (grade < 4) honorDiploma = false;
                 else if (grade == 5) countFive++;
             }
@@ -165,28 +174,23 @@ public class Gradebook {
         }
     }
 
-    private void computeScore() {
-
-        float sumOfGrades = 0.f;
-        this.scholarship = true;
-
-        for (var courses : gradeBook.keySet()) {
-            for (var subjects : gradeBook.get(courses).keySet()) {
-                var grade = gradeBook.get(courses).get(subjects).grade;
-                if (grade < 4) scholarship = false;
-                sumOfGrades += grade;
-            }
+    private void validateSubject(Integer semester, String subject) throws IllegalStateException {
+        if (gradeBook.get(semester) == null) {
+            throw new IllegalStateException("This semester doesn't exist in grade book");
         }
-        averageGradePoint = sumOfGrades / subjects;
-    }
-
-    private void handleSubject(Integer course, String subject) throws IllegalStateException {
-        if (gradeBook.get(course) == null) {
-            throw new IllegalStateException("This course doesn't exist in grade book");
-        }
-        if (gradeBook.get(course).get(subject) == null) {
+        if (gradeBook.get(semester).semesterSubjects.get(subject) == null) {
             throw new IllegalStateException("This subject doesn't exist in grade book");
         }
+    }
+
+    /**
+     * Getter of specific semester by its number
+     *
+     * @param semesterNumber is the number of semester
+     * @return current semester
+     */
+    public Semester getSemester(Integer semesterNumber) {
+        return gradeBook.get(semesterNumber);
     }
 
     /**
@@ -201,39 +205,28 @@ public class Gradebook {
     }
 
     /**
-     * Getter of the thesis.
+     * Getter of the thesis' grade.
      *
      * @return value of the thesis.
      */
-    public Integer getThesis() {
+    public Integer getThesisGrade() {
         return thesis;
     }
 
     /**
-     * Setter of the thesis.
+     * Setter of the thesis' grade.
      *
      * @param thesis is the required value of the thesis.
      */
-    public void setThesis(Integer thesis) {
+    public void setThesisGrade(Integer thesis) {
         this.thesis = thesis;
     }
 
     /**
-     * Getter of the scholarship.
-     *
-     * @return value of the scholarship.
+     * Enum-class for credit types of subjects
      */
-    public boolean getScholarShip() {
-        return scholarship;
-    }
-
-    /**
-     * Getter of the Average Grade Point.
-     *
-     * @return value of the GPA.
-     */
-    public Float getGPA() {
-        return averageGradePoint;
+    enum CreditType {
+        DIF_CREDIT, UNDIF_CREDIT, EXAM
     }
 
     /**
@@ -241,13 +234,75 @@ public class Gradebook {
      * and grade of the current subject. Used in my implementation
      * to prevent many HashMaps in Gradebook-class.
      */
-    private static class Subject {
-        private final String creditType;
+    static class Subject {
+        private final CreditType creditType;
         private Integer grade;
 
-        public Subject(String creditType) {
+        /**
+         * Main constructor of the subject.
+         *
+         * @param creditType is the required credit
+         *                   type of the subject.
+         */
+        public Subject(CreditType creditType) {
             this.creditType = creditType;
             this.grade = 0;
+        }
+    }
+
+    /**
+     * Semester-class. Contains map of all subjects which are contained
+     * in this semester and scholarship and Average Grade Point
+     * for this semester.
+     */
+    static class Semester {
+        private final Map<String, Subject> semesterSubjects;
+        private boolean scholarship;
+        private Float averageGradePoint;
+        private Integer amountOfSubjects;
+
+        /**
+         * Main constructor of Semester-class
+         */
+        public Semester() {
+            this.semesterSubjects = new HashMap<>();
+            this.scholarship = false;
+            this.averageGradePoint = 0.f;
+            this.amountOfSubjects = 0;
+        }
+
+        /**
+         * Getter of Average Grade Point of current semester.
+         *
+         * @return average grade point of current semester
+         */
+        public Float getAverageGradePoint() {
+            return this.averageGradePoint;
+        }
+
+        /**
+         * Getter of scholarship of current semester.
+         *
+         * @return scholarship of current semester.
+         */
+        public boolean getScolarship() {
+            return this.scholarship;
+        }
+
+        /**
+         * This method computes average grade point of current semester.
+         */
+        private void computeScore() {
+
+            float sumOfGrades = 0.f;
+            this.scholarship = true;
+
+            for (var subject : semesterSubjects.keySet()) {
+                var grade = semesterSubjects.get(subject).grade;
+                if (grade < 4) this.scholarship = false;
+                sumOfGrades += grade;
+            }
+            this.averageGradePoint = sumOfGrades / amountOfSubjects;
         }
     }
 }

8385498273505b10fa922a2fe8d6aefa37ee612f
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2022-12-19 17:12:53 +0700

    Update | I've written javadocs to all classes in my project.

--- task_1_5_1/src/main/java/ru/nsu/fit/tretyakov/ArityOperators.java
+++ task_1_5_1/src/main/java/ru/nsu/fit/tretyakov/ArityOperators.java
@@ -1,7 +1,19 @@
 package ru.nsu.fit.tretyakov;
+
 import java.util.Deque;
 
+/**
+ * This abstract class return array of exactly ARITY values of stack.
+ * I made this class to not write every time this function when user
+ * adds new arity operators.
+ */
 public abstract class ArityOperators {
+    /**
+     * @param ARITY           is the arity of the current operator-class.
+     * @param expressionStack is the stack of all operands in which
+     *                        this function will get an operand.
+     * @return the array of the operands in the expression stack.
+     */
     Number[] getValuesFromStack(int ARITY, Deque<Number> expressionStack) {
         Number[] operandsArray = new Number[ARITY];
         for (int i = 0; i < ARITY; i++) {
--- task_1_5_1/src/main/java/ru/nsu/fit/tretyakov/UnaryOperators.java
+++ task_1_5_1/src/main/java/ru/nsu/fit/tretyakov/UnaryOperators.java
@@ -2,9 +2,22 @@ package ru.nsu.fit.tretyakov;
 
 import java.util.Deque;
 
+/**
+ * Unary operators of the complex number. They transform value of the
+ * current complex number by specific rule.
+ */
 public abstract class UnaryOperators extends ArityOperators {
 
+    /**
+     * Sinus of the real value of the number.
+     */
     public static class Sin extends UnaryOperators implements Operator {
+        /**
+         * This function calculates sinus of the real number.
+         * @param expressionStack is the stack of all operands in which
+         *                        this function will get an operand.
+         * @return sinus of the real number.
+         */
         @Override
         public Number calculate(Deque<Number> expressionStack) {
             Number[] operands = getValuesFromStack(1, expressionStack);
@@ -12,7 +25,16 @@ public abstract class UnaryOperators extends ArityOperators {
         }
     }
 
+    /**
+     * Cosine of the real value of the number.
+     */
     public static class Cos extends UnaryOperators implements Operator {
+        /**
+         * This function calculates cosine of the real number.
+         * @param expressionStack is the stack of all operands in which
+         *                        this function will get an operand.
+         * @return cosine of the real number.
+         */
         @Override
         public Number calculate(Deque<Number> expressionStack) {
             Number[] operands = getValuesFromStack(1, expressionStack);
@@ -20,7 +42,16 @@ public abstract class UnaryOperators extends ArityOperators {
         }
     }
 
+    /**
+     * This class converts degree value to the radian value of the angle.
+     */
     public static class Rad extends UnaryOperators implements Operator {
+        /**
+         * This function converts degree value of the angle to the radian value.
+         * @param expressionStack is the stack of all operands in which
+         *                        this function will get an operand.
+         * @return radian value of the angle.
+         */
         @Override
         public Number calculate(Deque<Number> expressionStack) {
             Number[] operands = getValuesFromStack(1, expressionStack);
@@ -28,7 +59,17 @@ public abstract class UnaryOperators extends ArityOperators {
         }
     }
 
+    /**
+     * This class takes the square root of the number.
+     */
     public static class Sqrt extends UnaryOperators implements Operator {
+        /**
+         * This function calculates square root of the real number.
+         *
+         * @param expressionStack is the stack of all operands in which
+         *                        this function will get an operand.
+         * @return square root of the real number.
+         */
         @Override
         public Number calculate(Deque<Number> expressionStack) {
             Number[] operands = getValuesFromStack(1, expressionStack);

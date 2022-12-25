e61c2edb657d324fa946a38a4769592dac5f31bf
Tretiakovv <Tretiakovv@users.noreply.github.com>
2022-12-25 05:41:42 +0000

    Deploying to gh-pages from @ Tretiakovv/oop@b203e96b17302eb4b63533df45ee3f35148b2ebf 🚀

--- task_1_5_1/package-search-index.js
+++ task_1_5_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.fit.tretyakov"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.fit.tretyakov"},{"l":"ru.nsu.fit.tretyakov.operators"}];updateSearchResults();
